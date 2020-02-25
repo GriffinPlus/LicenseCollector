57cbf05bd33324dca7677b6b37a91b90072ec5d5
Sascha Falk <sascha@falk-online.eu>
2020-02-25 11:09:06 +0100

    Add project stub

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Griffin+")]
+[assembly: AssemblyProduct("Griffin+ License Collector")]
+[assembly: AssemblyCopyright("Copyright (c) Sebastian Piel 2020")]
---
+++ src/LicenseCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GriffinPlus.LicenseCollector
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
